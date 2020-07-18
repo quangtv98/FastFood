039447867e9e63c8f735a26923e8166f854a7f94
quangtv98 <51142758+quangtv98@users.noreply.github.com>
2020-07-18 14:22:14 +0700

    config file connect

--- lib/function/connect.php
+++ lib/function/connect.php
@@ -1,26 +1,19 @@
 <?php
-    // $host = "ec2-174-129-41-127.compute-1.amazonaws.com";
-    // $username = "bkpflgacsflhnj";
-    // $password = "717d981b62494b22548153d4f625021b6b6d09290993133528402109e74d8c7a";
-    // $dbname="d61ledlnrujbs6";
-    // try{
-    //     $conn = new PDO("mysql:host=$host; dbname=$dbname", $username, $password);
-    //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //     $conn->exec("set names utf8");
-    // }
-    // catch(PDOException $e){
-    //     echo "Lỗi kết nối đến server !!!".$e->getMessage();
-    // }
+
+    $host     = "d9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $username = "jxu1iwsjkwyds812";
+    $password = "rn8aj56rd3wmeema";
+    $dbname   = "j3eifrxpdhajx29h";
+    try{
+        $conn = new PDO("mysql:host=$host; dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->exec("set names utf8");
+    }
+    catch(PDOException $e){
+        echo "Lỗi kết nối đến server !!!".$e->getMessage();
+    }
 ?>
-<?php
-    // //goi namespace
-    // use MongoDB\Client;
-
-    // //require autoload
-    // require_once "vendor/autoload.php";
-
-    // // khoi tao class Client
-    // $conn = new Client("mongodb://127.0.0.1:27017");
+<!-- <?php
 
     $host = "localhost";
     $username = "root";
@@ -34,4 +27,4 @@
     catch(PDOException $e){
         echo "Lỗi kết nối đến server !!!".$e->getMessage();
     }
-?>
+?> -->
